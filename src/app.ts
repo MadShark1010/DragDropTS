baa46aa9ed1b6fb61a0ae83ac179ab0b2a4c18e8
MadShark1010 <mike.paynter@envoy.com>
2023-09-20 19:39:48 -0500

    added README file locally

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,22 @@ function autobind(_target: any, _methodName: string, descriptor: PropertyDescrip
     return adjDescriptor;
 }
 
+//ProjectList Class
+class ProjectList {
+
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+    }
+}
 
 
 //ProjectInput Class
