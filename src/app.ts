2ab79c23b8f9f233505da4dad75c1f7d9aafebb6
MadShark1010 <mike.paynter@envoy.com>
2023-09-20 20:26:51 -0500

    127 Rendering Project Lists

--- src/app.ts
+++ src/app.ts
@@ -48,13 +48,26 @@ class ProjectList {
     hostElement: HTMLDivElement;
     element: HTMLElement;
 
-    constructor() {
+    constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input';
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
     }
 }
 
@@ -143,3 +156,5 @@ class ProjectInput {
 }
 
 const renderedForm = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
