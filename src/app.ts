543d6f43e86fb5e118ce57feb9c430cb97be1c89
MadShark1010 <mike.paynter@envoy.com>
2023-09-28 18:55:11 -0500

    134: Start Drag and Drop

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 //Project Type
 enum ProjectStatus { Active, Finsihed }
 
+//Drag and Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHadler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
+
+// Project Type
 class Project {
 
     constructor(
@@ -125,7 +139,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project;
 
     get persons() {
@@ -144,7 +158,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure() {}
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        console.log(event);
+    }
+
+    dragEndHadler(_: DragEvent) {
+        console.log('DragEnd');
+    }
+
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHadler);
+    }
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
